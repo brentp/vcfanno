f6f52ad27b45ba4af5d732fe6c1a6b399e942f9d
Brent Pedersen (brentp) <bpederse@gmail.com>
2015-05-08 19:17:17 -0600

    more features in new version

--- vcfanno2.go
+++ vcfanno2.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"strconv"
 
 	"github.com/brentp/irelate"
+	"github.com/brentp/vcfgo"
 	"github.com/robertkrimen/otto"
 )
 
@@ -15,6 +17,7 @@ type Source struct {
 	Column int
 	Field  string
 	Index  int
+	IsJs   bool
 }
 
 func (s *Source) IsNumber() bool {
@@ -26,6 +29,24 @@ type Annotator struct {
 	Sources []Source
 }
 
+func (a *Annotator) JsOp(v *vcfgo.Variant, js string, vals []interface{}) interface{} {
+	a.vm.Set("chrom", v.Chrom())
+	a.vm.Set("start", v.Start())
+	a.vm.Set("end", v.End())
+	a.vm.Set("vals", vals)
+	value, err := a.vm.Run(js[3:])
+	if err != nil {
+		return fmt.Sprintf("js-error: %s", err)
+	}
+	val, err := value.ToString()
+	if err != nil {
+		log.Println("js-error:", err)
+		val = fmt.Sprintf("error:%s", err)
+	}
+	return val
+}
+
+/*
 func NewAnnotator(annos []anno, js string) *Annotator {
 	// TODO: figure out how to get annos from toml into here. flattend out to 1 op per source.
 	sources := make([]Source, 0)
@@ -35,6 +56,7 @@ func NewAnnotator(annos []anno, js string) *Annotator {
 	}
 	return &a
 }
+*/
 
 func (a *Annotator) partition(r irelate.Relatable) [][]irelate.Relatable {
 	parted := make([][]irelate.Relatable, 0)
@@ -50,10 +72,12 @@ func (a *Annotator) partition(r irelate.Relatable) [][]irelate.Relatable {
 
 func collect(v *irelate.Variant, rels []irelate.Relatable, src *Source, strict bool) []interface{} {
 	coll := make([]interface{}, 0)
-	// TODO: rels are all from same source, so we cold move the type checks outside the loop.
 	for _, other := range v.Related() {
+		if !overlap(v, other) {
+			continue
+		}
 		if o, ok := other.(*irelate.Variant); ok {
-			if !((strict && v.Is(o.Variant)) || (!strict && overlap(v, o))) {
+			if strict && !v.Is(o.Variant) {
 				continue
 			}
 
@@ -66,9 +90,6 @@ func collect(v *irelate.Variant, rels []irelate.Relatable, src *Source, strict b
 				coll = append(coll, val)
 			}
 		} else if o, ok := other.(*irelate.Interval); ok {
-			if !overlap(v, o) {
-				continue
-			}
 			val := o.Fields[src.Column-1]
 			if src.IsNumber() {
 				v, e := strconv.ParseFloat(val, 32)
@@ -80,13 +101,10 @@ func collect(v *irelate.Variant, rels []irelate.Relatable, src *Source, strict b
 				coll = append(coll, val)
 			}
 		} else if bam, ok := other.(*irelate.Bam); ok {
-			if !overlap(v, bam) {
-				continue
-			}
 			if bam.MapQ() < 1 || bam.Flags&(0x4) != 0 {
 				continue
 			}
-			coll = append(coll, bam)
+			coll = append(coll, 1)
 		} else {
 			panic(fmt.Sprintf("not supported for: %v", other))
 		}
@@ -98,11 +116,34 @@ func collect(v *irelate.Variant, rels []irelate.Relatable, src *Source, strict b
 func (a *Annotator) Annotate(r irelate.Relatable, strict bool) {
 
 	parted := a.partition(r)
-	v := r.(*irelate.Variant)
+	var b *irelate.Interval
+	var v *irelate.Variant
+	var isBed, isVariant bool
+	if v, isVariant = r.(*irelate.Variant); !isVariant {
+		if b, isBed = r.(*irelate.Interval); !isBed {
+			panic("can only annotate Bed or VCF at this time")
+		}
+		// create a Variant, annotate it, then pull out the info and put back
+		// into bed
+		m := make(vcfgo.InfoMap)
+		m["__order"] = []string{}
+		m["SVLEN"] = int(b.End()-b.Start()) - 1
+		var rels []irelate.Relatable
+		v = irelate.NewVariant(&vcfgo.Variant{Chromosome: b.Chrom(), Pos: uint64(b.Start() + 1),
+			Ref: "A", Alt: []string{"<DEL>"}, Info: m}, 0, rels)
+	}
 
 	for _, src := range a.Sources {
 		related := parted[src.Index]
 		vals := collect(v, related, &src, strict)
-		v.Info.Add(src.Name, Reducers[src.Op](vals))
+		if src.IsJs {
+			v.Info.Add(src.Name, a.JsOp(v.Variant, src.Op, vals))
+		} else {
+			v.Info.Add(src.Name, Reducers[src.Op](vals))
+		}
+	}
+	if isBed {
+		delete(v.Info, "SVLEN")
+		b.Fields = append(b.Fields, v.Info.String())
 	}
 }
