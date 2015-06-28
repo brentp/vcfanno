fb2b0647eae67a94925f9fddb19945a8e38de39e
Brent Pedersen (brentp) <bpederse@gmail.com>
2015-06-28 06:40:02 -0600

    add more to gem example

--- example/custom.js
+++ example/custom.js
@@ -9,3 +9,30 @@ function mean(vals) {
 function loc(chrom, start, end){
     return chrom + ":" + start + "-" + end
 }
+
+CLINVAR_LOOKUP = {'0': 'unknown',
+                  '1': 'germline',
+                  '2': 'somatic',
+                  '4': 'inherited',
+                  '8': 'paternal',
+                  '16': 'maternal',
+                  '32': 'de-novo',
+                  '64': 'biparental',
+                  '128': 'uniparental',
+                  '256': 'not-tested',
+                  '512': 'tested-inconclusive',
+                  '1073741824': 'other'}
+CLINVAR_SOURCE = {'0': 'unknown',
+                  '1': 'germline',
+                  '2': 'somatic',
+                  '4': 'inherited',
+                  '8': 'paternal',
+                  '16': 'maternal',
+                  '32': 'de-novo',
+                  '64': 'biparental',
+                  '128': 'uniparental',
+                  '256': 'not-tested',
+                  '512': 'tested-inconclusive',
+                  '1073741824': 'other'}
+
+
