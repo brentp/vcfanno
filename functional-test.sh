52470ceabcb28b5ba8a28c08fc16702c0c5b88f5
Brent Pedersen (brentp) <bpederse@gmail.com>
2015-10-13 16:32:33 -0600

    more tests

--- functional-test.sh
+++ functional-test.sh
@@ -19,9 +19,17 @@ show() {
 	echo -n "<## TEST.$_N ##>" $1
 }
 
-vcfanno -js example/custom.js example/conf.toml example/query.vcf.gz > obs
+vcfanno -js example/custom.js example/conf.toml example/query.vcf.gz > obs 2>err
 show "annotated vcf"
 check $(zgrep -cv ^# example/query.vcf.gz) $(grep -cv ^# obs)
 
 show "checking that header is updated"
 check "6" $(grep ^# obs | grep -c otto)
+
+
+vcfanno -ends -js example/custom.js example/conf.toml example/query.vcf.gz > obs 2>err
+show "checking that ends works"
+n=$(grep -v ^# obs | grep -c right_)
+check $( (( $n > 0 )) ) true
+n=$(grep -v ^# obs | grep -c left_)
+check $( (( $n > 0 )) ) true
