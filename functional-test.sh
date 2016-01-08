a695b7ed47362ec2040c02e1d72f65f899caa922
Brent Pedersen (brentp) <bpederse@gmail.com>
2016-01-08 14:00:24 -0700

    update tests

--- functional-test.sh
+++ functional-test.sh
@@ -2,10 +2,12 @@ test -e ssshtest || wget -q https://raw.githubusercontent.com/ryanlayer/ssshtest
 
 . ssshtest
 
+set -o nounset
+
 go install -race -a
 
 run check_example vcfanno -lua example/custom.lua example/conf.toml example/query.vcf.gz
-assert_equal $(zgrep -cv ^# example/query.vcf.gz) $(grep -cv ^# obs)
+assert_equal $(zgrep -cv ^# example/query.vcf.gz) $(grep -cv ^# $STDOUT_FILE)
 assert_equal 6 $(grep ^# $STDOUT_FILE | grep -c lua)
 assert_equal 2 $(grep -c "not found in" $STDERR_FILE)
 
@@ -16,10 +18,9 @@ assert_equal $(( $n > 0 )) 1
 n=$(grep -v ^# $STDOUT_FILE | grep -c left_)
 assert_equal $(( $n > 0 )) 1
 
-n=$(grep  ^"#CHROM" obs | cut -f 10- | wc -w) 
+n=$(grep  ^"#CHROM" $STDOUT_FILE | cut -f 10- | wc -w) 
 assert_equal $n 3
 
 
-# checking that all non-header lines have same number of columns
-n=$(grep  -v "^##" obs | awk 'BEGIN{FS="\t"}{ print NF}' | uniq)
+n=$(grep  -v "^##" $STDOUT_FILE | awk 'BEGIN{FS="\t"}{ print NF}' | uniq)
 assert_equal $n 12
